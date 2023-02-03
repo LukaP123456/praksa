02823c04b6c77e4efa0661094ac6b1014ffb9e4c
luka_prcic <bobsagott17@gmail.com>
2023-02-03 15:56:36 +0100

    guzzle

---
+++ guzzle_test/routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/guzzle', function () {
+    $client = new Client();
+    $res = $client->request('get', 'https://dummyjson.com/products/', [
+    ]);
+
+    $response = Http::get('https://dummyjson.com/products/');
+    dd($response);
+});
+
