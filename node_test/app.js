5e05ca815cb7edac60e6ce23a48ad7c20a527a9e
luka_prcic <bobsagott17@gmail.com>
2023-02-13 15:43:20 +0100

    async read and write

--- node_test/app.js
+++ node_test/app.js
@@ -1,11 +1,26 @@
-const path = require('path')
-console.log(path.sep)
-
-const filePath = path.join('/content', 'subfolder', 'test.txt')
-console.log(filePath)
-
-const baseName = path.basename(filePath)
-console.log(baseName)
-
-const absPath = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
-console.log(absPath)
+const {readFile, writeFile} = require('fs')
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first},${second}`,
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log(result)
+            }
+        )
+    })
+})
